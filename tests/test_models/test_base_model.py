c1992bb5170be94dcdc491e1d94fd1ccd09185f0
zfiromsa <mdzm841@gmail.com>
2024-02-10 04:32:51 -0800

    go

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,7 +11,25 @@ class Test_BaseModel(unittest):
         self.assertTrue(hasattr(obj, 'creat_at'))
         self.assertTrue(hasattr(obj, 'update_at'))
 
-        def test_id_generation(self):
-            obj1 = BaseModel()
-            obj2 = BaseModel()
-            self.assertnotEqual(obj1.id, obj2.id)
+    def test_id_generation(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertnotEqual(obj1.id, obj2.id)
+
+    def test_str_representation(self):
+        obj = BaseModel()
+        expe_str = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(str(obj), expe_str)
+
+    def test_to_dict_methode(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict[id], obj.id)
+        self.assertEqual(obj_dict['creat_at'], obj.created_at)
+        self.assertEqual(obj_dict['update_at'], obj.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
