6e3c6e76ef13ba435222d6cd79e9345325b8b386
zfiromsa <mdzm841@gmail.com>
2024-02-10 17:36:49 -0800

    go

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 """
 __initi___ methode for model package
 """
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
