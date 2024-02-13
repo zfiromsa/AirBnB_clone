ded9d2574acdb71d03adb5fff1de05369cdfaa2e
zfiromsa <mdzm841@gmail.com>
2024-02-12 22:41:28 -0800

    go

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ a class BaseModel that defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -38,7 +38,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """Update the instance of object with current date and time
