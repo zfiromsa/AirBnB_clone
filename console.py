331e765571a7ef12c87164600c862fe892cccb39
zfiromsa <mdzm841@gmail.com>
2024-02-12 23:17:29 -0800

    go

--- console.py
+++ console.py
@@ -6,20 +6,18 @@ from models.user import BaseModel
 """
 HBNBCommand class is command-line interpreter for  managing 
 public instance attributes:
-    prompt:
-    file:
+    prompt: str - the prompt displayed to the user
+
 public instance methods:
-    help(self, line):
-    do_quit(self, line):
-    do_EOF(self, line):
-    default(self, line):
-    emptyline(self):
+    help(self, line): Display a list of documented command.
+    do_quit(self, line): Quit command to exit the program.
+    do_EOF(self, line): EOF command to exit the program
+    default(self, line): Handles the command if its not recognized
+    emptyline(self): Overrides the default behavior when empty line is entered.
 """
 class HBNBCommand(Cmd):
     # intro = "Welcome to HBNBCommand. Type help or ? to list commands.\n"
     prompt = "(hbnb) "
-    file = None  # Used to demonstrate the use of file input/output
-
 
     def __init__(self):
         """Initializes a new instance of HBNBCommand class."""
