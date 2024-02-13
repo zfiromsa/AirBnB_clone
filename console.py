48072f7b1b39e1207ae403ae27f028cb31037b9c
zfiromsa <mdzm841@gmail.com>
2024-02-13 04:32:37 -0800

    go

--- console.py
+++ console.py
@@ -4,20 +4,22 @@ from cmd import Cmd
 from typing import IO
 from models.user import BaseModel
 
-"""
-HBNBCommand class is command-line interpreter for  managing 
-public instance attributes:
-    prompt: str - the prompt displayed to the user
-
-public instance methods:
-    help(self, line): Display a list of documented command.
-    do_quit(self, line): Quit command to exit the program.
-    do_EOF(self, line): EOF command to exit the program
-    default(self, line): Handles the command if its not recognized
-    emptyline(self): Overrides the default behavior when empty line is entered.
-"""
-
 class HBNBCommand(Cmd):
+    """
+    HBNBCommand class is command-line interpreter for  managing 
+
+    public instance attributes:
+        prompt: str - the prompt displayed to the user
+
+    public instance methods:
+        help(self, line): Display a list of documented command.
+        do_quit(self, line): Quit command to exit the program.
+        do_EOF(self, line): EOF command to exit the program
+        default(self, line): Handles the command if its not recognized
+        emptyline(self): Overrides the default behavior when empty line is entered.
+    """
+
+
     # intro = "Welcome to HBNBCommand. Type help or ? to list commands.\n"
     prompt = "(hbnb) "
 
