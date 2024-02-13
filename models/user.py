1a401bb5b2c785817cc6f7f6d63f1ad190475518
zfiromsa <mdzm841@gmail.com>
2024-02-13 05:08:41 -0800

    go

--- models/user.py
+++ models/user.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-
+"""A class User that inherits from BaseModel."""
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
+    """
+    User class represent user data and inherit from BaseModel.
+
+    Public class attribute:
+        email: str - empty.
+        password: str - empty.
+        first_name: str - empty.
+        last_name: str - empty.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialize a new instance of the User class.
+
+        Args:
+            args: Variable length argument list.
+            kwargs: Arbitrary keyword argument.
+        """
         super().__init__(*args, **kwargs)
-        self.email = kwargs.get("email", "")
-        self.password = kwargs.get("password", "")
-        self.first_name = kwargs.get("first_name", "")
-        self.last_name = kwargs.get("last_name", "")
+        self.email = kwargs.get('email', '')
+        self.password = kwargs.get('password', '')
+        self.first_name = kwargs.get('first_name', '')
+        self.last_name = kwargs.get('last_name', '')
